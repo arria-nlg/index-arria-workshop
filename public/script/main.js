3ad1cf746fb2c2cd6981aae5070426077e5db6bb
robert_hodgson_arria <robert.hodgson@arria.com>
2018-02-06 12:06:44 +0000

    handle user actions in front end

--- public/script/main.js
+++ public/script/main.js
@@ -1,6 +1,47 @@
-// get the portfolios
+// get the portfolios and add to select
+function loadPortfolios() {
+    getPortfolios()
+        .then(function (portfolios) {
+            console.log('Adding portfolios to select');
+            let select = document.getElementById('portfolio-select');
+            portfolios.forEach(function (portfolio) {
+                let option = document.createElement('OPTION');
+                option.setAttribute('value', portfolio.name);
+                option.appendChild(document.createTextNode(portfolio.name));
+                select.appendChild(option);
+            });
+            // enable the form
+            document.getElementById('portfolio-form').addEventListener('submit', submitPortfolio);
+            document.getElementById('portfolio-input').disabled = false;
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
+
+
+
+
+function submitPortfolio(event) {
+    event.preventDefault();
+    let e = document.getElementById('portfolio-select');
+    let ptf = e.options[e.selectedIndex].text;
+    console.log('submitting portfolio:' + ptf);
+    getPortfolioHoldings(ptf)
+        .then(function (holdings) {
+            addHoldingsTable(holdings);
+            return getNarrative(holdings);
+        }).then(function (narrative) {
+            let content = narrative[0].result;
+            addNarrative(content);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
 function getPortfolios() {
-    fetch('api/portfolios')
+    return fetch('api/portfolios')
         .then(function (response) {
             if (response.ok) {
                 return response.json();
@@ -8,15 +49,17 @@ function getPortfolios() {
             throw new Error('Returned status:' + response.status + '. Message: ' + response.statusText);
         }).then(function (data) {
             console.log(data);
+            return data.portfolios;
+
         }).catch(function (error) {
             console.log('There has been a problem getting your portfolios: ', error.message);
         });
 }
 
 // get the holdings and analytics
-function getHoldings() {
+function getPortfolioHoldings(portfolioName) {
     let holdings;
-    fetch('api/holdings' + '?portfolio=' + 'US_Fixed_Income_Ptf')
+    return fetch('api/holdings' + '?portfolio=' + portfolioName)
         .then(function (response) {
             // check the response
             if (response.ok) {
@@ -67,6 +110,7 @@ function getHoldings() {
                 holdings[h].positionValue = instValue * holdings[h].quantity;
             }
             console.log(holdings);
+            return holdings;
         })
         .catch(function (error) {
             console.log('There was a problem getting holdings: ', error.message);
@@ -122,7 +166,7 @@ function getNarrative() {
         }
     };
 
-    fetch('api/narrative', {
+    return fetch('api/narrative', {
         method: 'POST', // or 'PUT'
         body: JSON.stringify({ "arriaData": data.data }),
         headers: new Headers({
@@ -136,15 +180,62 @@ function getNarrative() {
                 return response.json();
             }
             throw new Error('Returned status:' + response.status + '. Message: ' + response.statusText);
-        }).then(function(narrative){
+        }).then(function (narrative) {
             console.log(narrative);
+            return narrative;
         })
-        .catch(function(error){
+        .catch(function (error) {
             console.log('There was a problem getting narrative: ', error.message);
         });
 
 }
 
-getPortfolios();
-getHoldings();
-getNarrative();
+
+function addHoldingsTable(holdings) {
+    // roll up the data
+    console.log('adding table');
+    // add a table of holdings to the page
+    // group by keys on the assets
+    // get portfolio total value
+    let ptfValue = holdings.reduce(function(r, v) {
+        return r + v.positionValue;
+    }, 0);
+
+    // group using d3 for ease
+    let nested = d3.nest()
+        .key(function (d) { return d.sectorName; })
+        .key(function (d) { return d.companyName; })
+        .entries(holdings);
+
+
+    // add the total portfolio
+    let wholePortfolio = [{"key":'Portfolio', "values":nested}]
+
+    // roll up the values
+    wholePortfolio.forEach(function(node) {
+        sumChildren(node, ptfValue);
+    });
+
+    console.log(wholePortfolio);
+
+    // add table
+    document.getElementById('holdings-table').innerText = JSON.stringify(wholePortfolio, null, 2);
+}
+
+function sumChildren(node, total) {
+      node.groupValue = node.values.reduce(function(r, v) {
+      return r + (v.values ? sumChildren(v, total) : v.positionValue);
+    },0);
+    node.groupWeight = node.groupValue / total;
+    return node.groupValue;
+  }
+
+function addNarrative(narrative) {
+    console.log('adding narrative');
+    // add the narrative to the page
+    let container = document.getElementById('narrative');
+    container.innerHTML = narrative;
+}
+
+loadPortfolios();
+
