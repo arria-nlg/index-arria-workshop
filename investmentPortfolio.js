1eba522a5815d5e524c5dc94136fe61d9b2f826d
William Bradshaw <william.bradshaw@arria.com>
2018-02-12 13:24:08 +0000

    Tidied up investmentPortfolio, adding help and comments. Includes a placeholder for delete, though this may not be possible.

--- investmentPortfolio.js
+++ investmentPortfolio.js
@@ -2,27 +2,20 @@ var request = require( 'request-promise' );
 var fs = require( 'fs-extra' );
 require('dotenv').config();
 
-
 READER_USER_ID = process.env.CRED_PORTFOLIO_USERID_R;
 READER_PASSWORD = process.env.CRED_PORTFOLIO_PWD_R;
 WRITER_USER_ID = process.env.CRED_PORTFOLIO_USERID_W;
 WRITER_PASSWORD = process.env.CRED_PORTFOLIO_PWD_W;
 
-/*
-READER_USER_ID = ""
-READER_PASSWORD = ""
-WRITER_USER_ID = ""
-WRITER_PASSWORD = ""
-*/
-
 BASE_URL = "https://investment-portfolio.mybluemix.net/"
 
-
-function getPortfolio() {
-  //get portoflios
-  console.log('get portfolios');
-  var hash = null;
-
+/*
+ * Gets a list of all portfolios in the system.
+ * Each portfolio has a name and the timestamp of when the portfolio was created
+ */
+function getPortfolios() {
+  console.log('Getting Portfolios');
+  
   // Request
   request( {
     method: 'GET',
@@ -42,6 +35,15 @@ function getPortfolio() {
   } );
 }
 
+/*
+ * Gets the holdings for a particular portfolio, e.g. what shares and options are owned.
+ * For every instrument the following information is returned:
+ *    asset        - What the holding is in. Typically the company name.
+ *    quantity     - How many holdings are owned in this instrument 
+ *    instrumentId - The unique identifier for the instrument.
+ *    companyName  - The name of the company.
+ *    sectorName   - The business sector the company is involved with, e.g. health, transport.
+ */
 function getPortfolioHoldings(portfolio) {
   //get holdings for the portfolio
   console.log('get holdings');
@@ -64,37 +66,70 @@ function getPortfolioHoldings(portfolio) {
   } );
 }
 
-
+/**
+ * Loads a new portfolio with the supplied name into the cloud service.
+ * The portfolio will be created without any holdings.
+ * If the portfolio already exists, nothing will change (i.e. existing holdings will remain). 
+ */
 function loadPortfolio(portfolioName) {
   //load portfolio
   console.log('load portfolios');
   var hash = null;
   var timeStamp = +new Date
 
-  // Request
+  // Check it doesn't already exist
   request( {
-    method: 'POST',
-    url: BASE_URL + 'api/v1/portfolios/',
+    method: 'GET',
+    url: BASE_URL + 'api/v1/portfolios/' + portfolioName + '/holdings',
     auth: {
-      username: WRITER_USER_ID,
-      password: WRITER_PASSWORD
+      username: READER_USER_ID,
+      password: READER_PASSWORD
     },
-    json: {
-      timestamp: timeStamp,
-      name: portfolioName,
-      closed: false,
-      data: {
-        manager: 'Will Smith'
-      }
-    }
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    qs: { latest: 'true' },
   } ).then( function( body ) {
-    console.log(body);
+    //Portfolio already exists
+      
+    console.log( 'A portfolio for ' + portfolioName + ' already exists');
+    console.log( 'Holdings:');
+    console.log( body );
+    return body;
   } ).catch( function( err ) {
-    console.log( err );
+    // Portfolio doesn't exist. Create one.
+      
+    // Request
+    request( {
+      method: 'POST',
+      url: BASE_URL + 'api/v1/portfolios/',
+      auth: {
+        username: WRITER_USER_ID,
+        password: WRITER_PASSWORD
+      },
+      json: {
+        timestamp: timeStamp,
+        name: portfolioName,
+        closed: false,
+        data: {
+          manager: 'John Smith'
+        }
+      }
+    } ).then( function( body ) {
+      console.log(body);
+      console.log("A new portfolio was created")
+    } ).catch( function( err ) {
+      console.log( err );
+    } );
   } );
 
 }
 
+/**
+ * Loads portfolio holdings from a json file and copies them into an existing portfolio.
+ * The loaded holdings replace any that have already been set for the portfolio.
+ * Throws an error if the portfolio does not already exist.
+ */
 function loadPortfolioHoldings(portfolio, holdingsJsonFile) {
   //load holdings into portfolo
   var timeStamp = +new Date
@@ -121,6 +156,36 @@ function loadPortfolioHoldings(portfolio, holdingsJsonFile) {
   } );
 }
 
+/**
+ *
+ */
+function deletePortfolio(portfolioName) {
+  //TODO
+}
+
+/**
+ * Displays a help message which shows the possible arguments and how to use them.
+ */
+function showHelp() {
+  console.log('Possible arguments:')
+  console.log(' -g <name>?           Gets currently loaded portfolios. If used without a name,'); 
+  console.log('                      this will show the names of all portfolios in the system.');
+  console.log('                      If you supply a portfolio name it will get the details of');
+  console.log('                      that portfolio.');
+  console.log();
+  console.log(' -l <name> -h? <data> Loads data into a named portfolio. Requires the name of ');
+  console.log('                      the portfolio to edit. If used without -h (holdings) this');
+  console.log('                      will create a blank portfolio. If used with -h this will ');
+  console.log('                      load the holdings data from the target json file into the');
+  console.log('                      portfolio. Please note that you can only load data into a');
+  console.log('                      portfolio which has already been created.');
+  console.log();
+  console.log(' -d <name>            Delete a named portfolio. Use caution, this is permanent.');
+  console.log();
+  console.log(' -help                Display these options.');
+                              
+}
+
 //get command-line args
 const args = process.argv
 
@@ -129,8 +194,8 @@ if (args[2]) {
   if (args[2] == '-l' ) {
     //load
     if(args[3]) {
-      //get portfolio name
       var portfolioName = args[3];
+      
       if(args[4]) {
         if (args[4] == '-h') {
           if(args[5]) {
@@ -146,32 +211,39 @@ if (args[2]) {
             });
           }
         } else {
-          console.log ('Try -h')
+          console.log(args[4] + ' is not a valid argument. Use -h to load holdings into a portfolio')
         }
       } else {
-        //load portfolio
-        console.log("load p " + portfolioName);
         loadPortfolio(portfolioName);
       }
-    }
-    else {
+    } else {
       console.log('Enter portfolio name')
     }
-  } else if (args[2] == '-g' ) {
+  } else if (args[2] == '-g') {
     //get
     if(args[3]) {
       portfolioName = args[3];
       getPortfolioHoldings(portfolioName);
     }
     else {
-      getPortfolio();
+      getPortfolios();
     }
 
   } else if (args[2] == '-d' ) {
-    deleteAllPortfolioHoldings();
+    //delete
+      if(args[3]){
+        deletePortfolio(args[3]);          
+      } else {
+        console.log('Enter portfolio name')
+      }
+
   } else {
-    console.log('Try -l or -g')
+    if (args[2] != '-help'){
+      console.log(args[2] + ' is not a valid argument');
+    }
+    showHelp();
   }
 }  else {
-  console.log('sTry -l or -g')
+  console.log("This script requires an argument")
+  showHelp();
 }
